fe088dac8d590536241b65d5c17b3edbed283a0b
Julio Tavares <jctbs@cin.ufpe.br>
2022-04-25 16:34:55 -0300

    feat: :sparkles: Convertendo Task para GraphQL

--- src/database/entities/Task.ts
+++ src/database/entities/Task.ts
@@ -1,24 +1,32 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { randomUUID as uuid } from 'node:crypto';
+import { Field, ObjectType } from 'type-graphql';
 
 @Entity('tasks')
+@ObjectType()
 export default class Task {
   @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
   id: string;
 
   @Column()
+  @Field(() => String)
   title: string;
 
   @Column()
+  @Field(() => String)
   description: string;
 
   @Column()
+  @Field(() => Boolean)
   is_complete: boolean
 
   @Column()
+  @Field(() => Date)
   created_at: Date
 
   @Column()
+  @Field(() => Date)
   updated_at: Date
 
   constructor() {
