5c272750be9f1fdd596e3c3b2a53f2076a22263e
Julio Tavares <jctbs@cin.ufpe.br>
2022-04-25 18:22:26 -0300

    feat: :sparkles: Crud Tasks

--- src/database/entities/Task.ts
+++ src/database/entities/Task.ts
@@ -1,32 +1,32 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, ID, ObjectType } from 'type-graphql';
 import { randomUUID as uuid } from 'node:crypto';
-import { Field, ObjectType } from 'type-graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('tasks')
 @ObjectType()
 export default class Task {
   @PrimaryGeneratedColumn('uuid')
-  @Field(() => String)
+  @Field(() => ID, { nullable: true })
   id: string;
 
   @Column()
-  @Field(() => String)
+  @Field(() => String, { nullable: false })
   title: string;
 
   @Column()
-  @Field(() => String)
+  @Field(() => String, { nullable: false })
   description: string;
 
   @Column()
-  @Field(() => Boolean)
+  @Field(() => Boolean, { nullable: true, name: "completed" })
   is_complete: boolean
 
   @Column()
-  @Field(() => Date)
+  @Field(() => Date, { nullable: true })
   created_at: Date
 
   @Column()
-  @Field(() => Date)
+  @Field(() => Date, { nullable: true  })
   updated_at: Date
 
   constructor() {
--- src/resolvers/Task.ts
+++ src/resolvers/Task.ts
@@ -1,11 +1,79 @@
-import { Query, Resolver } from "type-graphql";
+import Task from "../database/entities/Task";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import {getRepository, Repository} from "typeorm";
 
 
 @Resolver()
 export class TaskResolver {
 
+  private taskRepository: Repository<Task>
+
+  constructor() {
+    this.taskRepository = getRepository(Task);
+  }
+
   @Query(() => String)
   hello(): string {
     return "hello world!!";
   }
+
+  @Query(() => [Task], {name: 'findAll'})
+  async find(): Promise<Task[]> {
+    const tasks = await this.taskRepository.find();
+
+    return tasks
+  }
+
+  @Query(() => Task, {name: 'findTask'})
+  async findById(
+    @Arg("id") id: string
+  ): Promise<Task | undefined> {
+    const task = await this.taskRepository.findOne(id);
+
+    return task;
+  }
+
+  @Mutation(() => Task, {name: 'createTask'})
+  async create(
+    @Arg('title') title: string,
+    @Arg('description') description: string
+  ): Promise<Task> {
+    const task = this.taskRepository.create({title, description, is_complete: false});
+
+    await this.taskRepository.save(task)
+
+    return task
+  }
+
+  @Mutation(() => Boolean, {name: "deleteTaskById"})
+  delete(
+    @Arg('id') id: string
+  ): boolean {
+    
+    try {
+      this.taskRepository.delete({id})
+      return true;
+    } catch {
+      return false
+    }
+  }
+
+  @Mutation(() => Boolean, {name:"updateStatus"})
+  async updateStatus(
+    @Arg('id') id: string,
+  ): Promise<boolean> {
+    const task = await this.taskRepository.findOne(id);
+
+    if (!task) {
+      throw new Error ("Task not found")
+    }
+    try {
+      await this.taskRepository.update({id}, {is_complete: !task.is_complete});
+      return true
+    } catch {
+      return false
+    }
+
+  }
+  
 }
